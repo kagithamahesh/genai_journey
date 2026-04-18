2053ade8011b0fb6c4291424f555f26294af125f
Mahesh <kagithacm237@gmail.com>
2026-04-18 16:59:27 +0530

    add temprature,roles added

--- day4/ai_chat_gemini.py
+++ day4/ai_chat_gemini.py
@@ -1,16 +1,28 @@
 from google import genai
 
 # using provide the api key
-client = genai.Client(api_key="AIzaSyAf_pSxhQPthBI0O8afCts3wesdkG2GGMk")
+client = genai.Client(api_key="AIzaSyCsiQQG5XVNSkz_LTCaZISsj1_oRlLfmx0")
 
 target_model = "gemini-2.5-flash" 
 print(f"connecting model {target_model}...")
 
-response = client.models.generate_content(
-    model=target_model,
-   
-    contents="Explain Python like I am 10 years old"
-)
 
-print("\n🤖 AI Response:")
-print(response.text)
+
+try:
+
+    response = client.models.generate_content(
+        model=target_model,
+    
+         contents=[
+              {"role": "user", "parts": [{"text": "python"}]}
+         ],
+         config={
+        'temperature': 0.9  # High for creativity
+    }
+    )
+
+    print("\n🤖 AI Response:")
+    print(response.text)
+
+except Exception as e:
+    print("Error",e)
